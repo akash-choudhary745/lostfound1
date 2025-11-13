72674c123d868822457bef27878a7bdad4d2802e
Akash Choudhary <akashchoudhary6897@gmail.com>
2025-11-13 19:43:52 +0530

    Update MarkRecoveredServlet.java

--- src/java/servlet/MarkRecoveredServlet.java
+++ src/java/servlet/MarkRecoveredServlet.java
@@ -1,26 +1,4 @@
-//package servlet;
-//
-//import dao.ItemDAO;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.*;
-//import java.io.IOException;
-//
-//@WebServlet("/mark_recovered")
-//public class MarkRecoveredServlet extends HttpServlet {
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String idStr = req.getParameter("id");
-//        try {
-//            int id = Integer.parseInt(idStr);
-//            ItemDAO dao = new ItemDAO();
-//            dao.markAsRecovered(id);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        resp.sendRedirect(req.getContextPath() + "/department_panel");
-//    }
-//}
+
 
 
 package servlet;
